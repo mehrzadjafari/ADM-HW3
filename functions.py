a1872eec30defe952f52319cd696e0c4183ed929
ElisaValeriani <72880712+ElisaValeriani@users.noreply.github.com>
2020-11-25 13:58:34 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,6 +10,10 @@ import csv
 import re
 import os
 from langdetect import detect
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords 
+from nltk.stem import PorterStemmer
 
 
 
